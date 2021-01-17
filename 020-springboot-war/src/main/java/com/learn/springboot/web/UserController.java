cc89492346b87add8d011397d2e6612f3c49dac9
tanzhang666 <18717735973@163.com>
2021-01-17 21:53:50 +0800

    spring boot 打jar包和war包 有bug

---
+++ 020-springboot-war/src/main/java/com/learn/springboot/web/UserController.java
@@ -0,0 +1,21 @@
+package com.learn.springboot.web;
+
+
+import com.learn.springboot.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/index")
+    public String userDetail(Model model){
+        User user=new User();
+        user.setId(1L);
+        user.setName("test");
+        user.setAge(20);
+        model.addAttribute("user",user);
+        return "index";
+    }
+}
---
+++ 020-springboot-war/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangtan
+  Date: 2021/1/17
+  Time: 19:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>user detail</title>
+</head>
+<body>
+    <div>${user.name}</div>
+    <div>${user.age}</div>
+</body>
+</html>
